2c282012d8968fc3791af4c93e4c9934404048cd
Vladyslav Varvianskyi <pol92leon@gmail.com>
2022-12-16 21:43:55 +0200

    Add navigation component

---
+++ src/shared-components/NavigationBar.jsx
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react';
+import { NavLink } from 'react-router-dom';
+import { ROUTES } from '../constants/router';
+import classes from './NavigationBar.module.scss';
+
+const NavigationBar = () => {
+    return (
+        <Fragment>
+            <nav className={classes.nav__container}>
+                <ul className={classes.nav__items}>
+                    <li className={classes.nav__item}>
+                        <NavLink to={{ pathname: ROUTES.currencyExchange }}>Currency</NavLink>
+                    </li>
+                    <li className={classes.nav__item}>
+                        <NavLink to={{ pathname: ROUTES.quiz }}>Quiz</NavLink>
+                    </li>
+                    <li className={classes.nav__item}>
+                        <NavLink to={{ pathname: ROUTES.photogramm }}>PhotoGramm</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </Fragment>
+    );
+};
+
+export default NavigationBar;
