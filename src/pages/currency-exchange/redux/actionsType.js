5885e7a04e5703c0cd62584c8d37ded56816012a
Vladyslav Varvianskyi <pol92leon@gmail.com>
2023-01-06 15:21:07 +0200

    Change to redux-react currencyExchange

---
+++ src/pages/currency-exchange/redux/actionsType.js
@@ -0,0 +1,5 @@
+export const actionType = Object.freeze({
+    reverseCurrencies: 'REVERSE_CURRENCIES',
+    changeCurrenciesFrom: 'CHANGE_CURRENCIES_FROM',
+    changeCurrenciesTo: 'CHANGE_CURRENCIES_TO',
+});
--- src/pages/currency-exchange/redux/currency.reducer.js
+++ src/pages/currency-exchange/redux/currency.reducer.js
@@ -1,7 +1,40 @@
-const initState = {};
+import { CURRENCIES, CURRENCY_RATES } from '../constants';
+import { actionType } from './actionsType';
+
+const initState = {
+    currencies: CURRENCIES,
+    currencyRates: CURRENCY_RATES,
+    currencyFrom: CURRENCIES.uah,
+    currencyTo: CURRENCIES.usd,
+};
 
 function reducerCurrency(state = initState, action) {
+    switch (action.type) {
+        case actionType.reverseCurrencies: {
+            const to = state.currencyTo;
+            const from = state.currencyFrom;
 
+            return {
+                ...state,
+                currencyFrom: to,
+                currencyTo: from,
+            }
+        }
+        case actionType.changeCurrenciesFrom: {
+            return {
+                ...state,
+                currencyFrom: action.currency,
+            };
+        }
+        case actionType.changeCurrenciesTo: {
+            return {
+                ...state,
+                currencyTo: action.currency,
+            };
+        }
+        default:
+            return state;
+    }
 }
 
 export default reducerCurrency;
