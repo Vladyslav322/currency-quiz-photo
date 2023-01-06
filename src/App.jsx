b13eeac2416d7414e278aef1a7e648c43db17048
Vladyslav Varvianskyi <pol92leon@gmail.com>
2023-01-06 15:24:10 +0200

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import { NavigationBar } from './shared-components';
 const App = () => {
     return (
         <div className="App">
-            <NavigationBar />
+            <NavigationBar/>
 
             <Routes>
-                <Route path="/" element={<Navigate to={ROUTES.currencyExchange}/>}/>
-                <Route path={ROUTES.currencyExchange} element={<CurrencyExchange/>}/>
-                <Route path={ROUTES.quiz} element={<QuizApp/>}/>
-                <Route path={ROUTES.photogramm} element={<PhotoGramm/>}/>
+                <Route path="/" element={ <Navigate to={ ROUTES.currencyExchange }/> }/>
+                <Route path={ ROUTES.currencyExchange } element={ <CurrencyExchange/> }/>
+                <Route path={ ROUTES.quiz } element={ <QuizApp/> }/>
+                <Route path={ ROUTES.photogramm } element={ <PhotoGramm/> }/>
             </Routes>
         </div>
     );
