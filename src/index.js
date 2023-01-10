08bd919dfebec1529427dbde189d8ead27fcd66b
Vladyslav Varvianskyi <pol92leon@gmail.com>
2023-01-10 17:02:48 +0200

    Changes

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './pages/root-reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={ store }>
-        <HashRouter>
+        <BrowserRouter>
             <App/>
-        </HashRouter>
+        </BrowserRouter>
     </Provider>
 );
